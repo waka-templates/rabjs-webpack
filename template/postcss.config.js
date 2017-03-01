234977de397cdd942587d103d8a74c357ca5a1be
ximing <ximing@meituan.com>
2017-03-01 20:20:48 +0800

    post message

---
+++ template/postcss.config.js
@@ -0,0 +1,13 @@
+
+/**
+ * Created by yeanzhi on 17/1/13.
+ */
+'use strict';
+module.exports = {
+    plugins: [
+        require('postcss-clearfix')(),
+        require('autoprefixer')(),
+        require('postcss-position')(),
+        require('postcss-size')()
+    ]
+}
